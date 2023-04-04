552416dc83bbd2a7b7bc3aa18bb2765a078cb6a3
rafel-akp <rafelinoxz7@gmail.com>
2023-04-04 11:54:56 +0800

    model, service, and helper for category

---
+++ helper/model.go
@@ -0,0 +1,23 @@
+package helper
+
+import (
+	"rafelck/go-restful-api/model/domain"
+	"rafelck/go-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryReponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponses []web.CategoryResponse
+
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
+	}
+
+	return categoryResponses
+}
